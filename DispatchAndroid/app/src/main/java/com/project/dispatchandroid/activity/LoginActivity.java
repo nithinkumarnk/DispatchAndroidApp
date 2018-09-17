bae0159e5afd9897109ec466a0fdde926c52caae
nithin.k <nithin.k@tataelxsi.co.in>
2018-09-17 17:23:45 +0530

    Initial login changes

---
+++ DispatchAndroid/app/src/main/java/com/project/dispatchandroid/activity/LoginActivity.java
@@ -0,0 +1,239 @@
+package com.project.dispatchandroid.activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChooseMfaContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ForgotPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.NewPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
+import com.project.dispatchandroid.R;
+import com.project.dispatchandroid.utils.AppHelper;
+
+import java.util.List;
+import java.util.Locale;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AlertDialog userDialog;
+    private ProgressDialog waitDialog;
+
+    // Screen fields
+    private EditText inUsername;
+    private EditText inPassword;
+
+    //Continuations
+    private MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation;
+    private ForgotPasswordContinuation forgotPasswordContinuation;
+    private NewPasswordContinuation newPasswordContinuation;
+    private ChooseMfaContinuation mfaOptionsContinuation;
+
+    // User Details
+    private String username;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize application
+        AppHelper.init(getApplicationContext());
+        initApp();
+        findCurrent();
+    }
+
+    // Login if a user is already present
+    public void logIn(View view) {
+        signInUser();
+    }
+
+    // App methods
+    // Register user - start process
+    public void signUp(View view) {
+        signUpNewUser();
+    }
+
+    private void signUpNewUser() {
+        Intent registerActivity = new Intent(this, RegisterUserActivity.class);
+        startActivityForResult(registerActivity, 1);
+    }
+
+    // initialize app
+    private void initApp() {
+        inUsername = (EditText) findViewById(R.id.editTextUserId);
+        inUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if(s.length() == 0) {
+                    TextView label = (TextView) findViewById(R.id.textViewUserIdLabel);
+                    label.setText(R.string.Username);
+                    inUsername.setBackground(getDrawable(R.drawable.text_border_selector));
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView label = (TextView) findViewById(R.id.textViewUserIdMessage);
+                label.setText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0) {
+                    TextView label = (TextView) findViewById(R.id.textViewUserIdLabel);
+                    label.setText("");
+                }
+            }
+        });
+
+        inPassword = (EditText) findViewById(R.id.editTextUserPassword);
+        inPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if(s.length() == 0) {
+                    TextView label = (TextView) findViewById(R.id.textViewUserPasswordLabel);
+                    label.setText(R.string.Password);
+                    inPassword.setBackground(getDrawable(R.drawable.text_border_selector));
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView label = (TextView) findViewById(R.id.textViewUserPasswordMessage);
+                label.setText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0) {
+                    TextView label = (TextView) findViewById(R.id.textViewUserPasswordLabel);
+                    label.setText("");
+                }
+            }
+        });
+    }
+
+    private void signInUser() {
+        username = inUsername.getText().toString();
+        if(username == null || username.length() < 1) {
+            TextView label = (TextView) findViewById(R.id.textViewUserIdMessage);
+            label.setText(inUsername.getHint()+" cannot be empty");
+            inUsername.setBackground(getDrawable(R.drawable.text_border_error));
+            return;
+        }
+
+        AppHelper.setUser(username);
+
+        password = inPassword.getText().toString();
+        if(password == null || password.length() < 1) {
+            TextView label = (TextView) findViewById(R.id.textViewUserPasswordMessage);
+            label.setText(inPassword.getHint()+" cannot be empty");
+            inPassword.setBackground(getDrawable(R.drawable.text_border_error));
+            return;
+        }
+
+        showWaitDialog("Signing in...");
+        AppHelper.getPool().getUser(username).getSessionInBackground(authenticationHandler);
+    }
+
+    private void showWaitDialog(String message) {
+        closeWaitDialog();
+        waitDialog = new ProgressDialog(this);
+        waitDialog.setTitle(message);
+        waitDialog.show();
+    }
+
+    private void findCurrent() {
+        CognitoUser user = AppHelper.getPool().getCurrentUser();
+        username = user.getUserId();
+        if(username != null) {
+            AppHelper.setUser(username);
+            inUsername.setText(user.getUserId());
+            user.getSessionInBackground(authenticationHandler);
+        }
+    }
+
+    AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice device) {
+            Log.d(TAG, " -- Auth Success");
+            AppHelper.setCurrSession(cognitoUserSession);
+            AppHelper.newDevice(device);
+            closeWaitDialog();
+            launchUser();
+        }
+
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String username) {
+            closeWaitDialog();
+            Locale.setDefault(Locale.US);
+            getUserAuthentication(authenticationContinuation, username);
+        }
+
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+            closeWaitDialog();
+            mfaAuth(multiFactorAuthenticationContinuation);
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            closeWaitDialog();
+            TextView label = (TextView) findViewById(R.id.textViewUserIdMessage);
+            label.setText("Sign-in failed");
+            inPassword.setBackground(getDrawable(R.drawable.text_border_error));
+
+            label = (TextView) findViewById(R.id.textViewUserIdMessage);
+            label.setText("Sign-in failed");
+            inUsername.setBackground(getDrawable(R.drawable.text_border_error));
+
+            showDialogMessage("Sign-in failed", AppHelper.formatException(e));
+        }
+
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
+            /**
+             * For Custom authentication challenge, implement your logic to present challenge to the
+             * user and pass the user's responses to the continuation.
+             */
+            if ("NEW_PASSWORD_REQUIRED".equals(continuation.getChallengeName())) {
+                // This is the first sign-in attempt for an admin created user
+                newPasswordContinuation = (NewPasswordContinuation) continuation;
+                AppHelper.setUserAttributeForDisplayFirstLogIn(newPasswordContinuation.getCurrentUserAttributes(),
+                        newPasswordContinuation.getRequiredAttributes());
+                closeWaitDialog();
+                firstTimeSignIn();
+            } /*else if ("SELECT_MFA_TYPE".equals(continuation.getChallengeName())) {
+                closeWaitDialog();
+                mfaOptionsContinuation = (ChooseMfaContinuation) continuation;
+                List<String> mfaOptions = mfaOptionsContinuation.getMfaOptions();
+                selectMfaToSignIn(mfaOptions, continuation.getParameters());
+            }*/
+        }
+    };
+
+    private void closeWaitDialog() {
+        try {
+            waitDialog.dismiss();
+        }
+        catch (Exception e) {
+            //
+        }
+    }
+}
